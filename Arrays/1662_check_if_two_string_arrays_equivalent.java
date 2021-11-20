3063aa8d2ac3ddc455b504d993342c097875fdd6
Abhishek <abhikr756@gmail.com>
2021-11-20 23:21:02 +0530

    problem 1662

---
+++ Arrays/1662_check_if_two_string_arrays_equivalent.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2)
+    {
+    if(String.join("",word1).equals(String.join("",word2)))
+        {
+        return true;
+        }
+    else
+        {
+        return false;
+        }
+    }
+}

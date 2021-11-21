02c263c1d80c37b81b684be5fdcee442b74f4e89
Abhishek <abhikr756@gmail.com>
2021-11-21 12:16:18 +0530

    pattern5

---
+++ Basics/pattern5.java
@@ -0,0 +1,16 @@
+4import java.util.Scanner;
+public class pattern5{
+    public static void main(String[] args) {
+        System.out.println("enter the value for which pattern is to be printed");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(" " +i);
+            }
+            System.out.println();
+        }
+    }
+}

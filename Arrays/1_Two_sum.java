72604f4e5c2b5d9e13d5effb817c083eac20cada
Abhishek <abhikr756@gmail.com>
2021-11-28 22:32:48 +0530

    Problem 1

---
+++ Arrays/1_Two_sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int answer[]=new int[2];
+        int count = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if((nums[i]+nums[j])==target)
+                {
+                    answer[0] = i;
+                    answer[1] = j;
+                    count++;
+                    break;
+                }
+            }
+               if(count > 0)
+                {
+                    break;
+                }
+        }
+        return answer;
+    }
+}

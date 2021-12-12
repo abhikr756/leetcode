0843a99bea53131d27495752be460bbb7bbe43a4
Abhishek <abhikr756@gmail.com>
2021-12-12 19:05:41 +0530

    Problem 771

---
+++ Arrays/771_jewels_and_stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char c : stones.toCharArray()) 
+        {
+            if (jewels.contains(c + ""))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}

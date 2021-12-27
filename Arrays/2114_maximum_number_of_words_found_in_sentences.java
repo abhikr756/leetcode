73392125305fd42c8f2210b53c4fd175fd3874d9
Abhishek <abhikr756@gmail.com>
2021-12-27 21:22:30 +0530

    Problem 2114

---
+++ Arrays/2114_maximum_number_of_words_found_in_sentences.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+		int res = 0;
+		for (String sentence: sentences) 
+        {
+			res = Math.max(res, sentence.split(" ").length);
+		}
+		return res;
+	}
+}

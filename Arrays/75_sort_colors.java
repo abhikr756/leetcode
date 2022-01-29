c29a4d4656642eb5889e69722f32fe68b26fa1e7
Abhishek <abhikr756@gmail.com>
2022-01-29 20:37:46 +0530

    Problem 75

---
+++ Arrays/75_sort_colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int count[] = new int[3];
+        for (int i: nums) 
+        {
+            count[i]++;
+        }
+        for (int i = 0; i < nums.length; i++) 
+        {
+            if (i < count[0]) 
+            {
+                nums[i] = 0;
+            } 
+            else if (i < count[0] + count[1])
+            {
+                nums[i] = 1;
+            } 
+            else 
+            {
+                nums[i] = 2;
+            }
+        }
+    }
+}

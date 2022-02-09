20a3247641b8234311211eefaee68984271f6982
Abhishek <abhikr756@gmail.com>
2022-02-09 23:58:18 +0530

    Problem 204

--- Numbers/204_count_prime.java
+++ Numbers/204_count_prime.java
@@ -1,15 +1,15 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean[] notPrime = new boolean[n];
+        boolean[] prime = new boolean[n];
         int count = 0;
-        for (int i = 2; i < n; i++) 
+        for(int i=2;i<n;i++)
         {
-            if (notPrime[i] == false) 
+            if(prime[i]==false)
             {
                 count++;
-                for (int j = 2; i*j < n; j++) 
+                for(int j=2*i;j<n;j=j+i)
                 {
-                    notPrime[i*j] = true;
+                    prime[j]=true;
                 }
             }
         }

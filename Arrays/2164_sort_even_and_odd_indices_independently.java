19e8c10708c51dc03e8bbdc5fa242d23f4479eeb
Abhishek <abhikr756@gmail.com>
2022-02-24 22:59:12 +0530

    Problem 2164

---
+++ Arrays/2164_sort_even_and_odd_indices_independently.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+      if(nums.length<3)
+          return nums;
+     List<Integer>even=new ArrayList<Integer>();
+     List<Integer>odd=new ArrayList<Integer>();
+        int ans[]=new int[nums.length];
+     for(int i=0;i<nums.length;i++)
+     {
+         if(i%2==0)
+         {
+             even.add(nums[i]);
+         }
+         else
+         {
+             odd.add(nums[i]);
+         }
+     }
+        Collections.sort(even);
+        Collections.sort(odd);
+        Collections.reverse(odd);
+        int j=0,k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i%2==0)
+            {
+                ans[i]=even.get(j++);
+            }
+            else
+            {
+                ans[i]=odd.get(k++);
+            }
+        }
+        return ans;
+    }
+}

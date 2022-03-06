b21fe6819c0cd637f9c53bb37e45edc0e7b03d6c
Abhishek <abhikr756@gmail.com>
2022-03-06 23:13:06 +0530

    Problem 153

---
+++ Arrays/153_find_minimum_in_rotated_sorted_array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l=0,r=nums.length-1;
+         int mid=0;
+         while(l<r){
+            if(nums[l]<nums[r])
+            { 
+                return nums[l];
+            }
+            mid=(r+l)/2;
+
+          if(nums[mid]<nums[l]){
+              r=mid;
+          }
+
+          else{
+
+              l=mid+1;
+          }
+        }
+
+        return nums[r];
+        
+    }
+}

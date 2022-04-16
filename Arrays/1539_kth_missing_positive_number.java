e5e85f554d6ae6ef35f39b330e05b12e97032248
Abhishek <abhikr756@gmail.com>
2022-04-16 22:51:29 +0530

    Problem 1539

---
+++ Arrays/1539_kth_missing_positive_number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+      for(int a: arr)
+      {
+          if(a<=k)
+          {
+              k++;
+          }
+      }
+        return k;
+    }
+}

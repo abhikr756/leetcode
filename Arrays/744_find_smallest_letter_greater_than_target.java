b86bf4538a515b648267e8017ce7b4a3a55f2442
Abhishek <abhikr756@gmail.com>
2022-04-16 23:07:26 +0530

    Problem 744

--- Arrays/744_find_smallest_letter_greater_than_target.java
+++ Arrays/744_find_smallest_letter_greater_than_target.java
@@ -1,10 +1,10 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        int start=0;
-        int end=letters.length;
-        while(start<end)
+       int start = 0;
+        int end = letters.length-1;
+        while(start<=end)
         {
-            int mid=start+(end-start)/2;
+            int mid = start+(end-start)/2;
             if(letters[mid]<=target)
             {
                 start=mid+1;

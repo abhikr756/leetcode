2b234297a7c798a33db696f1e40fb055e648c0fa
Abhishek <abhikr756@gmail.com>
2022-04-29 23:03:00 +0530

    Problem 1935

---
+++ String/1935_maximum_number_of_words_you_can_type.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+      int c = 0;
+    String str[] = text.split(" ");
+    for(int i = 0;i<str.length;i++)
+    {
+        boolean flag = false;
+        for(int j = 0;j<brokenLetters.length();j++)
+        {
+            if(str[i].contains(brokenLetters.charAt(j)+""))
+            {
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            c++;
+    }
+    return c;
+    }
+}

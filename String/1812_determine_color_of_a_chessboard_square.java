bd3a8a7aece4c470fb0ecc71ec807bb78c9e1761
Abhishek <abhikr756@gmail.com>
2022-04-30 23:15:52 +0530

    Problem 1812

---
+++ String/1812_determine_color_of_a_chessboard_square.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        char ch[]=coordinates.toCharArray();
+        int alphachar=ch[0]-'0';
+        int numericchar=ch[1]-'0';
+        int merge=alphachar+numericchar;
+        if(merge%2==0)
+        {
+            return false;
+        }
+        return true;
+    }
+}

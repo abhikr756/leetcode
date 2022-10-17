9f51a53160cf5172c13708456403331a43485a1c
Abhishek <abhikr756@gmail.com>
2022-10-17 21:19:18 +0530

    Problem 821

---
+++ String/821_shortest_distance_to_a_character.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+      List<Integer> list = new ArrayList<>();
+        char c1[] = s.toCharArray();
+        for(int i = 0;i<c1.length;i++){
+            if(c1[i] == c){
+                list.add(i);
+            }
+        }
+        int res[] = new int[c1.length];
+        for(int i = 0;i<res.length;i++){
+            int min = Integer.MAX_VALUE;
+            for(int p : list){
+                min = Math.min(min, Math.abs(i-p));
+            }
+            res[i] = min;
+        }
+        return res;
+    }
+}

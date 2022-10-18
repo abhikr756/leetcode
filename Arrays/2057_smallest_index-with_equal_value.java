c07e582cb8ccf2ecb6b7eb29d755dd3a8dcc1933
Abhishek <abhikr756@gmail.com>
2022-10-18 21:47:10 +0530

    Problem 2057

---
+++ Arrays/2057_smallest_index-with_equal_value.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        int res =-1;
+        for(int i=0;i<nums.length;i++){
+            if(i%10==nums[i]){
+                return i;
+            }
+        }
+        return res;
+    }
+}

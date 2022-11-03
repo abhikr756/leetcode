48ebe3f53823443a33ec45c92295060604fa1bd0
Abhishek <abhikr756@gmail.com>
2022-11-03 22:25:36 +0530

    Problem 1640

---
+++ Arrays/1640_check_array_formation_through_concatenation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        StringBuilder sb = new StringBuilder();
+        for(int x : arr){
+			sb.append("#");
+            sb.append(x);
+            sb.append("#");
+        }
+        for(int i = 0; i < pieces.length; i++){
+            StringBuilder res = new StringBuilder();
+            for(int j = 0; j < pieces[i].length; j++){
+				res.append("#");
+                res.append(pieces[i][j]);
+                res.append("#");
+            }
+            if(!sb.toString().contains(res.toString())){
+                return false;
+            }
+        }
+        return true;
+    }
+}

c1e8b69934a50aa054cdf86a5b6970d315d8b87d
Abhishek <abhikr756@gmail.com>
2022-11-27 23:27:28 +0530

    Problem 2465

---
+++ Arrays/2465_number_of_distinct_averages.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int distinctAverages(int[] nums) {
+     Set<Integer> seen = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0, n = nums.length; i < n / 2; ++i) {
+            seen.add(nums[i] + nums[n - 1 - i]);
+        }
+        return seen.size();   
+    }
+}
